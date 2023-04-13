19be118107143c6edd200452a9e948c487525baa
erkam_home <erkam.uzuncayir@fauderfield.com>
2023-04-13 16:24:04 +0300

    Player.cs: Modify Player script for newly created PlayerAnimator script
    Due to animation transition logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
 
+    private bool _isWalking;
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -29,8 +30,15 @@ public class Player : MonoBehaviour
         Vector3 playerMovement = moveDir * (moveSpeed * Time.deltaTime);
         transform.position += playerMovement;
 
+        _isWalking = moveDir != Vector3.zero;
+        
         const float rotate_speed = 10f;
         Vector3 playerRotate = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotate_speed);
         transform.forward = playerRotate;
+    }
+
+    public bool IsWalking()
+    {
+        return _isWalking;
     }
 }
