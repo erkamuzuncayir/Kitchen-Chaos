2f8bddd6894d5a6b7c9d02274551a5bb08ea38b5
erkam_desktop <erkamuzuncayir@gmail.com>
2023-04-14 00:50:06 +0300

    Player: Add collision detection logic into player script.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// TODO: This code will be refactored and replaced with new Input System.
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
@@ -11,13 +10,60 @@ public class Player : MonoBehaviour
     private bool _isWalking;
 
     private void Update()
-    
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+    
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        Vector3 playerMovement = moveDir * (moveSpeed * Time.deltaTime);
-        transform.position += playerMovement;
 
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
+            playerRadius, moveDir, moveDistance);
+
+        if (!canMove)
+        {
+            /*
+             * Cannot move towards moveDir
+             * Attempt only X movement
+             */
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
+                playerRadius, moveDirX, moveDistance);
+
+            if (canMove)
+            {
+                // Can move only on the X
+                moveDir = moveDirX;
+            }
+            else
+            {
+                /*
+                 * Cannot move only on the X
+                 * Attempt only Z movement
+                 */
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
+                    playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    // Can move only on the Z
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    // Cannot move in any direction
+                }
+            }
+        }
+
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
+        
         _isWalking = moveDir != Vector3.zero;
         
         const float rotate_speed = 10f;
