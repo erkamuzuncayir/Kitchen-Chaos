7831d3a952b788fdd04f2ba400ccf284e5287a7a
erkam_desktop <erkamuzuncayir@gmail.com>
2023-04-14 02:58:39 +0300

    Player.cs: Modify and complete collision detection logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
-    
+    [SerializeField] private LayerMask countersLayerMask;
+
     private bool _isWalking;
+    private Vector3 _lastInteractDir;
 
     private void Update()
     {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    public bool IsWalking()
+    {
+        return _isWalking;
+    }
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+    
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDir != Vector3.zero)
+        {
+            _lastInteractDir = moveDir;
+        }
+        const float _INTERACT_DISTANCE = 2f;
+        if (Physics.Raycast(transform.position, _lastInteractDir, out RaycastHit raycastHit, _INTERACT_DISTANCE, countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                // Has ClearCounter
+                clearCounter.Interact();
+            }
+        }
+    }
+    private void HandleMovement()
+    {
+        
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
     
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
@@ -69,10 +104,6 @@ public class Player : MonoBehaviour
         const float rotate_speed = 10f;
         Vector3 playerRotate = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotate_speed);
         transform.forward = playerRotate;
-    }
-
-    public bool IsWalking()
-    {
-        return _isWalking;
+    
     }
 }
