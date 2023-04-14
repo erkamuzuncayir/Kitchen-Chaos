ef493fa6a090a8b0588924c69ace1a07a56a3713
erkam_desktop <erkamuzuncayir@gmail.com>
2023-04-14 03:16:10 +0300

    GameInput: Create event based Interact handling logic

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,15 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
+    public event EventHandler OnInteractAction;
     private PlayerInputActions _playerInputActions;
 
     private void Awake()
     {
         _playerInputActions = new PlayerInputActions();
         _playerInputActions.Player.Enable();
+
+        _playerInputActions.Player.Interact.performed += InteractPerformed;
+    }
+
+    private void InteractPerformed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        OnInteractAction?.Invoke(this, EventArgs.Empty);
     }
     
     public Vector2 GetMovementVectorNormalized()
