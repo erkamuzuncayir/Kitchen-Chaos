2cd17fbbd8156f23c6d5759006fed7f3f3db22f4
erkam_desktop <erkamuzuncayir@gmail.com>
2023-04-17 23:20:44 +0300

    Implement object pick up and drop logic with updating ClearCounter.cs and ContainerCounter.cs

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -9,5 +9,23 @@ public class ClearCounter : BaseCounter
     
     public override void Interact(Player player)
     {
+        if (!HasKitchenObject())
+        {
+            if (player.HasKitchenObject())
+            {
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+        }
+        else
+        {
+            if (player.HasKitchenObject())
+            {
+                
+            }
+            else
+            {
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
