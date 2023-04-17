2730158e41728fa2d7b72654ceb7586642182035
erkam_desktop <erkamuzuncayir@gmail.com>
2023-04-17 16:12:45 +0300

    Create KitchenObject script to reference to KitchenObjectSO

---
+++ Assets/Scripts/KitchenObject.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitchenObject : MonoBehaviour
+{
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
+    private ClearCounter _clearCounter;
+    public KitchenObjectSO GetKitchenObjectSO()
+    {
+        return kitchenObjectSO;
+    }
+
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        _clearCounter = clearCounter;
+    }
+
+    public ClearCounter GetClearCounter()
+    {
+        return _clearCounter;
+    }
+}
