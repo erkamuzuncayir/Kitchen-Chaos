c7111fe55fdeb68a50b16aac72f279f1177a97cc
erkam_desktop <erkamuzuncayir@gmail.com>
2023-04-19 17:47:46 +0300

    Create PlateCompleteVisual.cs for visualizing ingredients on plate

---
+++ Assets/Scripts/PlateCompleteVisual.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateCompleteVisual : MonoBehaviour
+{
+    [Serializable]
+    public struct KitchenObjectSO_GameObject
+    {
+        public KitchenObjectSO KitchenObjectSO;
+        public GameObject GameObject;
+    }
+    
+    [SerializeField] private PlateKitchenObject plateKitchenObject;
+    [SerializeField] private List<KitchenObjectSO_GameObject> _kitchenObjectSOGameObjectsList;
+    private void Start()
+    {
+        plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
+
+        foreach (KitchenObjectSO_GameObject kitchenObjectSOGameObject in _kitchenObjectSOGameObjectsList)
+        {
+            kitchenObjectSOGameObject.GameObject.SetActive(false);
+        }
+    }
+
+    private void PlateKitchenObject_OnIngredientAdded(object sender, PlateKitchenObject.OnIngredientAddedEventArgs e)
+    {
+        foreach (KitchenObjectSO_GameObject kitchenObjectSOGameObject in _kitchenObjectSOGameObjectsList)
+        {
+            if (kitchenObjectSOGameObject.KitchenObjectSO == e.KitchenObjectSO)
+            {
+                kitchenObjectSOGameObject.GameObject.SetActive(true);
+            }
+        }
+    }
+}
