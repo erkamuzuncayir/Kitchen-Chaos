ca72674b17c4cf61bea96d0e31870790a43a54d7
erkam_desktop <erkamuzuncayir@gmail.com>
2023-04-21 01:13:26 +0300

    Create GameOverUI.cs, GamePlayingClockUI.cs

---
+++ Assets/Scripts/UI/GamePlayingClockUI.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePlayingClockUI : MonoBehaviour
+{
+    [SerializeField] private Image _timerImage;
+
+    private void Update()
+    {
+        _timerImage.fillAmount = KitchenGameManager.Instance.GetGamePlayingTimerNormalized();
+    }
+}
