3aec552cd265c61dc0a0f159ef1d061f5fa5736c
Erkam <erkam@fedora>
2023-04-21 22:46:30 +0300

    Create Loader.cs, LoaderCallback.cs, and MainMenuUI.cs for main menu section

---
+++ Assets/Scripts/LoaderCallback.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoaderCallback : MonoBehaviour
+{
+    // private bool _isFirstUpdate = true;
+
+    private void Start()
+    {
+        Loader.LoaderCallback();
+    }
+    
+    /*
+    private void Update()
+    {
+        if (_isFirstUpdate)
+        {
+            _isFirstUpdate = false;
+            
+        }
+    }
+    */
+}
